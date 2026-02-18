c754f81828c296ef032b9835e4ae56d51aef27d4
MarkoEv <markodevp@gmail.com>
2026-02-18 12:28:15 -0600

    Add header template content html

---
+++ src/templates/header.js
@@ -0,0 +1,17 @@
+export const Header = () => {    
+    const view = `
+    <nav>
+        <ul class="grid grid-cols-2 text-center justify-center pt-1.5">
+            <li>
+                <a href="#" class="text-2xl text-stone-50">Rick y Morty</a>
+            </li>
+            <li>
+                <a href="#" class="text-2xl text-stone-50">Home</a>
+            </li>
+        </ul>
+    </nav>
+    `;
+    return view;
+
+}
+
